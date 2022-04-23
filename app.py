25931d9284201065599f9ed0a401a5880dc95114
MorxTheGamer <zamarwint11@gmail.com>
2022-04-23 15:34:14 -0500

    Fixes Again

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ import sys, os
 class App:
   def __init__(self):
     self.app = Frame()
+    self.unsafe_methods = ["login", "request", "setup", "save_game_iq"]
+    self.methods = ["introduction", "check_iq", "change_difficulty", "sign_up", "delete_account", "start"]
+
+  def getMethods(self):
+    for index, method in enumerate(self.methods):
+      print(f"{index}. {method}")
 
   def start(self):
     os.system("clear")
     print("Welcome to The Official Math Games Terminal App!")
-    unsafe_methods = ["login", "request", "setup", "save_game_iq"]
     while True:
       try:
-        execution = input("What would you like to do?\n> ")
-        for method in unsafe_methods:
+        execution = input(f"What would you like to do?\n{self.getMethods()}\n>")
+        for method in self.unsafe_methods:
           if (execution == method):
             print("Invaild option.")
             continue
